91cb9b8b5634d673f6bbf5708d521b1b80534bb3
snapav <goglesasha08@gmail.com>
2025-02-20 18:18:53 +0300

    Рекламная кампания

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '<h1> Миссия Колонизация Марса </h1>'
@@ -10,6 +11,15 @@ def index():
 @app.route('/index')
 def about():
     return '<h1> И на Марсе будут яблони цвести! </h1>'
+
+
+@app.route('/promotion')
+def promotion():
+    return '''<h1>Человечество вырастает из детства. <br>
+Человечеству мала одна планета. <br>
+Мы сделаем обитаемыми безжизненные пока планеты. <br>
+И начнем с Марса! <br>
+Присоединяйся!</h1>'''
 
 
 if __name__ == '__main__':
