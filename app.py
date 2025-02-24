a8a87ecf1c59efe5ea66f69e3c49171710ab50d4
snapav <goglesasha08@gmail.com>
2025-02-24 17:53:12 +0300

    Реклама с картинкой

--- app.py
+++ app.py
@@ -25,7 +25,9 @@ def promotion():
 def image_mars():
     return render_template('image_mars.html')
 
-
+@app.route('/promotion_image')
+def promotion_image():
+    return render_template('promotion_image.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
