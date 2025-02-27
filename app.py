e15413df000a0232b43578dd8c828af8b513a618
snapav <goglesasha08@gmail.com>
2025-02-27 19:01:04 +0300

    Автоматический ответ

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from lib2to3.fixes.fix_input import context
-from os import lstat
 
 from flask import Flask, render_template, request
 
@@ -24,35 +23,43 @@ def promotion():
 И начнем с Марса! <br>
 Присоединяйся!</h1>'''
 
+
 @app.route('/image_mars')
 def image_mars():
     return render_template('image_mars.html')
+
 
 @app.route('/promotion_image')
 def promotion_image():
     return render_template('promotion_image.html')
 
-@app.route('/astronaut_selection' , methods=['GET', 'POST'])
+
+@app.route('/astronaut_selection', methods=['GET', 'POST'])
 def astronaut_selection():
-    if request.method == 'GET':
-        return render_template('astronaut_selection.html')
-    elif request.method == 'POST':
-        print(request.form['surname'])
-        print(request.form['name'])
-        print(request.form['education'])
-        print(request.form['profession'])
-        print(request.form['motivation'])
-        print(request.form['gender'])
-        print(request.form['ready'])
-        print(request.form['email'])
-        return '<h1> Анкета отправлена </h1>'
+    return render_template('astronaut_selection.html')
+
+
+@app.route('/answer', methods=['POST'])
+@app.route('/auto_answer', methods=['POST'])
+def answer():
+    context = {
+        'surname': request.form['surname'],
+        'name': request.form['name'],
+        'education': request.form['education'],
+        'profession': ', '.join(request.form.getlist('profession')),
+        'gender': request.form['gender'],
+        'motivation': request.form['motivation'],
+        'ready': request.form.get('ready', '') == 'Готов'
+    }
+    return render_template('answer.html', **context)
 
 @app.route('/training/<prof>')
 def training(prof):
-    context ={
+    context = {
         'prof': prof
     }
-    return  render_template('training.html', **context)
+    return render_template('training.html', **context)
+
 
 @app.route('/list_prof/<lst>')
 def list_prof(lst):
@@ -62,6 +69,7 @@ def list_prof(lst):
 
     }
     return render_template('list_prof.html', **context)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
